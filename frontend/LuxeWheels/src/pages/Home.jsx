af94562d2b12baff27ea65119f3a61b04b2f236a
Ananthu007-AK <ananthukrishna1122@gmail.com>
2025-02-19 12:19:00 +0530

    validation

--- frontend/LuxeWheels/src/pages/Home.jsx
+++ frontend/LuxeWheels/src/pages/Home.jsx
@@ -7,46 +7,46 @@ import Login from './Login'
 function Home() {
   return (
     <>
-  <meta charSet="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Luxewheels - Where Elegance Meets The Road</title>
-  <link rel="stylesheet" href="Home.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Michroma&display=swap"
-    rel="stylesheet"
-  />
-  <nav>
-    <a href="/" className="logo">
-      Luxewheels
-    </a>
-    <div className="nav-links">
-      <a href="#">Buy</a>
-      <a href="#">Rent</a>
-      <a href="#">Sell</a>
-      <a href="#">About us</a>
-      <a href="./Login">Login</a>
-    </div>
-  </nav>
-  <main>
-    <section className="hero">
-      <h1>Where Elegance Meets The Road</h1>
-      <input
-        type="text"
-        className="search-bar"
-        placeholder="Tell us your dream..."
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Luxewheels - Where Elegance Meets The Road</title>
+      <link rel="stylesheet" href="Home.css" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Michroma&display=swap"
+        rel="stylesheet"
       />
-      <div className="car-showcase">
-        <img
-          src={img1}
-          alt="Luxury car showcase"
-          className="car-image"
-        />
-      </div>
-    </section>
-  </main>
-</>
+      <nav>
+        <a href="/" className="logo">
+          Luxewheels
+        </a>
+        <div className="nav-links">
+          <a href="#">Buy</a>
+          <a href="#">Rent</a>
+          <a href="#">Sell</a>
+          <a href="#">About us</a>
+          <a href="./Login">Login</a>
+        </div>
+      </nav>
+      <main>
+        <section className="hero">
+          <h1>Where Elegance Meets The Road</h1>
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Tell us your dream..."
+          />
+          <div className="car-showcase">
+            <img
+              src={img1}
+              alt="Luxury car showcase"
+              className="car-image"
+            />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
