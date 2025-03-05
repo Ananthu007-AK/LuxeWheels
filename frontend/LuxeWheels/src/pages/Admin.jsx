c7a0c0a222a430297de12011f5abdfbb909fc4b5
Ananthu007-AK <ananthukrishna1122@gmail.com>
2025-03-05 12:35:29 +0530

    adjustment

--- frontend/LuxeWheels/src/pages/Admin.jsx
+++ frontend/LuxeWheels/src/pages/Admin.jsx
@@ -7,10 +7,7 @@ function Admin() {
   return (
     <>
       <>
-  <meta charSet="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>LuxeWheels Admin Dashboard</title>
-  <link rel="stylesheet" href="no.css" />
   <div className="container">
     {/* Sidebar */}
     <div className="sidebar">
