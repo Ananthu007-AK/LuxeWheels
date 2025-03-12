0fa50ae725c2b769e591a4b1e1118dcdd8aafd1b
Ananthu007-AK <ananthukrishna1122@gmail.com>
2025-03-12 23:07:57 +0530

    just a dot

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ function verifyToken(req,res,next){
     if(!token)
         return res.status(401).json({error:'Access Denied'});
     try{
-        const decoded=jwt.verify(token.process.env.JWT_SECRET);
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
         req.userid=decoded.userid;
         next()
     }
